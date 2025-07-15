a9ca726e2f6e3ac34e557198f1b9ae68efc6a521
milodev <milodev09@gmail.com>
2025-07-14 23:50:41 -0500

    add: controllers presentation layer

---
+++ src/Presentation/UrlShortener.Services.API/Controllers/UrlController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using UrlShortener.Application.UseCases.Commands;
+using UrlShortener.Application.UseCases.DTOs;
+
+namespace UrlShortener.Services.API.Controllers
+{
+    [ApiController]
+    [Route("api/url")]
+    public class UrlController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UrlController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("shorten")]
+        public async Task<ActionResult> CreateShortUrl([FromBody] CreateShortUrlCommand command)
+        {
+            var result = await _mediator.Send(command);
+            if (!result)
+            {
+                return BadRequest(new ProblemDetails
+                {
+                    Title = "Error",
+                    Detail = "Failed to create short URL. Please check the provided long URL.",
+                    Status = StatusCodes.Status400BadRequest
+                });
+            }
+
+            return Created();
+        }
+    }
+}
