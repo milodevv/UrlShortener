1908210b8509870358434815cb144f4c9e8a429e
milodev <milodev09@gmail.com>
2025-07-14 23:50:09 -0500

    add: dependency injection

---
+++ src/Infrastructure/UrlShortener.Persistence/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using UrlShortener.Application.UseCases.Interfaces;
+using UrlShortener.Persistence.Contexts;
+using UrlShortener.Persistence.Interceptors;
+using UrlShortener.Persistence.Services;
+
+namespace UrlShortener.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseMySql(configuration.GetConnectionString("MySqlConnection"),
+                    ServerVersion.AutoDetect(configuration.GetConnectionString("MySqlConnection")),
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName))
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+            });
+            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            services.AddScoped<IUrlShorteningService, UrlShorteningService>();
+            return services;
+        }
+    }
+}
