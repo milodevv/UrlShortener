599162bf162c1a94d006b829451a7200c30201e2
milodev <milodev09@gmail.com>
2025-07-14 23:47:12 -0500

    add: dependency injection

---
+++ src/Application/UrlShortener.Application.UseCases/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using UrlShortener.Application.UseCases.Mappings;
+using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Application.UseCases
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.AddMaps(typeof(MappingsProfile).Assembly);
+                cfg.AddMaps(typeof(ShortenedUrl).Assembly);
+            });
+
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
