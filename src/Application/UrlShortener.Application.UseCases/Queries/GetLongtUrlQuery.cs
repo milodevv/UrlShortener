5fdc122f5f5e9973f1dd5c8972f13e15c3b3b716
milodev <milodev09@gmail.com>
2025-07-15 05:03:05 -0500

    add: get long url query

---
+++ src/Application/UrlShortener.Application.UseCases/Queries/GetLongtUrlQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using UrlShortener.Application.UseCases.DTOs;
+
+namespace UrlShortener.Application.UseCases.Queries
+{
+    public sealed record GetLongtUrlQuery : IRequest<LongUrlResponseDTO>
+    {
+        public string Code { get; set; }
+    }
+}
