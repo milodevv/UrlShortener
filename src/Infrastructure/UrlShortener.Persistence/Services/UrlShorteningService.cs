ffcc2350bcd58c17f70e691b9927d979316c1b27
milodev <milodev09@gmail.com>
2025-07-14 23:49:47 -0500

    services implementation

---
+++ src/Infrastructure/UrlShortener.Persistence/Services/UrlShorteningService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Application.UseCases.DTOs;
+using UrlShortener.Application.UseCases.Interfaces;
+
+namespace UrlShortener.Persistence.Services
+{
+    public class UrlShorteningService : IUrlShorteningService
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly Random _random = new();
+
+        public UrlShorteningService(IApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<string> GenerateUniqueCode()
+        {
+            var codeChars = new char[ShortLinkSettingsDTO.Length];
+            int maxValue = ShortLinkSettingsDTO.Alphabet.Length;
+
+            while (true)
+            {
+                for (var i = 0; i < ShortLinkSettingsDTO.Length; i++)
+                {
+                    var randomIndex = _random.Next(maxValue);
+
+                    codeChars[i] = ShortLinkSettingsDTO.Alphabet[randomIndex];
+                }
+
+                var code = new string(codeChars);
+
+                if (!await _dbContext.ShortenedUrls.AnyAsync(s => s.Code == code))
+                {
+                    return code;
+                }
+            }
+        }
+    }
+}
