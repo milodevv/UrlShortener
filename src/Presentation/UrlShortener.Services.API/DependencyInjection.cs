1e154477d59084c74401adc2ba5e69e3fbc630ae
milodev <milodev09@gmail.com>
2025-07-14 23:51:09 -0500

    add: dependency injection

---
+++ src/Presentation/UrlShortener.Services.API/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿namespace UrlShortener.Services.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentationServices(this IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+            return services;
+        }
+    }
+}
