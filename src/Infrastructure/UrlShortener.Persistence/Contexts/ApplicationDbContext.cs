b3d7e96d79cb54e693358774fc2df66f7bbffeeb
milodev <milodev09@gmail.com>
2025-07-14 23:48:18 -0500

    add: context

---
+++ src/Infrastructure/UrlShortener.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Reflection.Emit;
+using UrlShortener.Application.UseCases.DTOs;
+using UrlShortener.Application.UseCases.Interfaces;
+using UrlShortener.Domain.Entities;
+using UrlShortener.Persistence.Interceptors;
+
+namespace UrlShortener.Persistence.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>, IApplicationDbContext
+    {
+        private readonly AuditableEntitySaveChangesInterceptor _auditableEntitySaveChangesInterceptor;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, AuditableEntitySaveChangesInterceptor auditableEntitySaveChangesInterceptor) : base(options)
+        {
+            _auditableEntitySaveChangesInterceptor = auditableEntitySaveChangesInterceptor;
+        }
+
+        public DbSet<ShortenedUrl> ShortenedUrls { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            builder.Entity<CreateShortUrlResponseDTO>().HasNoKey();
+            builder.Entity<CreateShortUrlResponseDTO>().ToView(null);
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditableEntitySaveChangesInterceptor);
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
