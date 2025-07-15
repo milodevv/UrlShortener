2d9234f4bc6ce7cf4b352e1d5b033b0f4ec0f73e
milodev <milodev09@gmail.com>
2025-07-15 05:06:42 -0500

    add: representation configure options service

---
+++ src/Infrastructure/UrlShortener.Persistence/Configurations/ShortLinkSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace UrlShortener.Persistence.Configurations
+{
+    public class ShortLinkSettings
+    {
+        public int Length { get; set; }
+        public string Alphabet { get; set; } = string.Empty;
+    }
+}
