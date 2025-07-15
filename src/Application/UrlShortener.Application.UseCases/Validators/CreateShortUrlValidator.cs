a775090205bb39cd1f8c0eaf0a29b6b704d146cf
milodev <milodev09@gmail.com>
2025-07-14 23:46:36 -0500

    add: validators

---
+++ src/Application/UrlShortener.Application.UseCases/Validators/CreateShortUrlValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using UrlShortener.Application.UseCases.Commands;
+
+namespace UrlShortener.Application.UseCases.Validators
+{
+    public class CreateShortUrlValidator : AbstractValidator<CreateShortUrlCommand>
+    {
+        public CreateShortUrlValidator()
+        {
+            RuleFor(x => x.LongUrl).NotEmpty().NotNull().Must(BeAValidUrl).WithMessage("The provided URL is not valid");
+        }
+
+        private bool BeAValidUrl(string url)
+        {
+            return Uri.TryCreate(url, UriKind.Absolute, out var uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+    }
+}
