45f1efd613ef5e2653851c950da7bc9f435fe0e5
milodev <milodev09@gmail.com>
2025-07-14 23:48:50 -0500

    add: interceptors

---
+++ src/Infrastructure/UrlShortener.Persistence/Interceptors/AuditableEntitySaveChangesInterceptor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace UrlShortener.Persistence.Interceptors
+{
+    public class AuditableEntitySaveChangesInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
