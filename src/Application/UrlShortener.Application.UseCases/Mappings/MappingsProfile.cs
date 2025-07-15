454b87b45e4c36eccb11b49caae728743e0eac90
milodev <milodev09@gmail.com>
2025-07-14 23:46:12 -0500

    add: mappings

---
+++ src/Application/UrlShortener.Application.UseCases/Mappings/MappingsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using UrlShortener.Application.UseCases.Commands;
+using UrlShortener.Application.UseCases.DTOs;
+using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Application.UseCases.Mappings
+{
+    public class MappingsProfile : Profile
+    {
+        public MappingsProfile()
+        {
+            CreateMap<CreateShortUrlCommand, ShortenedUrl>().ReverseMap();
+            CreateMap<ShortenedUrl, CreateShortUrlResponseDTO>().ReverseMap();
+        }
+    }
+}
