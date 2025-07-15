a59b82ff98885944486e9bfe3b45769617050a29
milodev <milodev09@gmail.com>
2025-07-14 23:41:55 -0500

    add: entites and commons

---
+++ src/Domain/UrlShortener.Domain/Commons/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace UrlShortener.Domain.Commons;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+}
---
+++ src/Domain/UrlShortener.Domain/Entities/ShortenedUrl.cs
@@ -0,0 +1,10 @@
+﻿using UrlShortener.Domain.Commons;
+
+namespace UrlShortener.Domain.Entities;
+
+public class ShortenedUrl : BaseAuditableEntity
+{
+    public string LongUrl { get; set; } = string.Empty;
+    public string ShortUrl { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
+}
