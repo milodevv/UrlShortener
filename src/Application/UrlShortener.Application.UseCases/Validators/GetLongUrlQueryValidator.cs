750cc9821ff939f6ba9e163340c5f26c912cdf2f
milodev <milodev09@gmail.com>
2025-07-17 00:37:08 -0500

    add: new get long url validator

---
+++ src/Application/UrlShortener.Application.UseCases/Validators/GetLongUrlQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using UrlShortener.Application.UseCases.Queries;
+
+namespace UrlShortener.Application.UseCases.Validators
+{
+    public class GetLongUrlQueryValidator : AbstractValidator<GetLongtUrlQuery>
+    {
+        public GetLongUrlQueryValidator()
+        {
+            RuleFor(x => x.Code).NotNull().NotEmpty().Must(code => code.Length == 9).WithMessage("Invalid code");
+        }
+    }
+}
