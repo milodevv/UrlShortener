437f41d5fe09ed7e627a19847e1649b372b44bef
milodev <milodev09@gmail.com>
2025-07-19 03:29:54 -0500

    mod: file name

---
+++ src/Application/UrlShortener.Application.UseCases/Queries/GetLongUrlQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using UrlShortener.Application.UseCases.DTOs;
+
+namespace UrlShortener.Application.UseCases.Queries
+{
+    public sealed record GetLongUrlQuery : IRequest<LongUrlResponseDTO>
+    {
+        public string Code { get; set; }
+    }
+}
