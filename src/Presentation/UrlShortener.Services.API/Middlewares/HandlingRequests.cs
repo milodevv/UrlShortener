1113e0fbb2be768a53fa525aeeab8cb62a6605df
milodev <milodev09@gmail.com>
2025-08-02 18:36:35 -0500

    add: intercept request and request response

---
+++ src/Presentation/UrlShortener.Services.API/Middlewares/HandlingRequests.cs
@@ -0,0 +1,27 @@
+﻿namespace UrlShortener.Services.API.Middlewares
+{
+    public class HandlingRequests
+    {
+        private readonly RequestDelegate _next;
+
+        public HandlingRequests(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine($"NUEVA PETICION HACIA LA RUTA: {context.Request.Path}, CON CUERPO: {context.Request.Body.ToString()}");
+            await _next(context);
+            Console.WriteLine($"RESPUESTA DE LA PETICION HACIA LA RUTA: {context.Request.Path}, CON CODIGO DE ESTADO: {context.Response.StatusCode}");
+        }
+    }
+
+    public static class HandlingRequestsExtensions
+    {
+        public static IApplicationBuilder UseHandlingRequests(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HandlingRequests>();
+        }
+    }
+}
